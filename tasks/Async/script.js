71b7fd3c98629cef9620d3dcf2fde79daf2e6878
Igor Romanchuk <defhn900@gmail.com>
2021-05-28 19:32:10 +0300

    add function

--- tasks/Async/script.js
+++ tasks/Async/script.js
@@ -1,99 +1,119 @@
 const container = document.querySelector('.container')
 
-const weatherError = document.createElement('div')
-weatherError.className = 'weather_error'
+const renderForm = () => {
+  const weatherForm = document.createElement('div')
+  weatherForm.className = 'weather_form'
 
-const weatherForm = document.createElement('div')
-weatherForm.className = 'weather_form'
+  const inputCity = document.createElement('input')
+  inputCity.type = 'text'
+  inputCity.name = 'city'
+  inputCity.placeholder = 'Enter your city here...'
 
-container.append(weatherForm, weatherError)
+  const inputCountry = document.createElement('input')
+  inputCountry.type = 'text'
+  inputCountry.name = 'country'
+  inputCountry.placeholder = 'Enter your country here...'
 
-const inputCity = document.createElement('input')
-inputCity.type = 'text'
-inputCity.name = 'city'
-inputCity.placeholder = 'Enter your city here...'
+  const buttonCreate = document.createElement('button')
+  buttonCreate.innerText = 'Submit'
+  buttonCreate.className = 'weather_submit'
 
-const inputCountry = document.createElement('input')
-inputCountry.type = 'text'
-inputCountry.name = 'country'
-inputCountry.placeholder = 'Enter your country here...'
+  const weatherError = document.createElement('div')
+  weatherError.className = 'weather_error'
+  weatherError.innerText = 'Error. Please fill in both lines'
 
-const button = document.createElement('button')
-button.innerText = 'Submit'
-button.className = 'weather_submit'
+  weatherForm.append(inputCity, inputCountry, buttonCreate)
+  container.append(weatherForm, weatherError)
+}
 
-weatherForm.append(inputCity, inputCountry, button)
+renderForm()
 
-const weatherInfo = document.createElement('div')
-weatherInfo.className = 'weather_info'
+const renderWeatherContainer = () => {
+  const weatherContainer = document.createElement('div')
+  weatherContainer.id = 'weather_container'
+  container.append(weatherContainer)
+}
 
-container.append(weatherInfo)
+renderWeatherContainer()
 
-const weatherImg = document.createElement('div')
-weatherImg.className = 'weather_img'
+const renderWeather = (data) => {
+  const weatherImg = document.createElement('div')
+  weatherImg.className = 'weather_img'
+  weatherImg.style.background = `url(${data.current.weather_icons})`
 
-const weatherTemperature = document.createElement('div')
-weatherTemperature.className = 'weather_temperature'
+  const weatherTemperature = document.createElement('div')
+  weatherTemperature.className = 'weather_temperature'
+  weatherTemperature.innerText = `${data.current.temperature}°C`
 
-const weatherLocation = document.createElement('div')
-weatherLocation.className = 'weather_location'
+  const weatherLocation = document.createElement('div')
+  weatherLocation.className = 'weather_location'
+  weatherLocation.innerText = `${data.location.name}, ${data.location.country}`
 
-const weatherData = document.createElement('div')
-weatherData.className = 'weather_data'
+  const weatherData = document.createElement('div')
+  weatherData.className = 'weather_data'
 
-weatherInfo.append(weatherImg, weatherTemperature, weatherLocation, weatherData)
+  const dataTime = document.createElement('div')
+  dataTime.className = 'data_time'
+  dataTime.innerText = `${data.current.observation_time}`
 
-const dataTime = document.createElement('div')
-dataTime.className = 'data_time'
+  const dataFeelsLike = document.createElement('div')
+  dataFeelsLike.className = 'data_feels-like'
+  dataFeelsLike.innerText = `Feels like: ${data.current.feelslike}°C`
 
-const dataFeelsLike = document.createElement('div')
-dataFeelsLike.className = 'data_feels-like'
+  const dataTodayIs = document.createElement('div')
+  dataTodayIs.className = 'data_today-is'
+  dataTodayIs.innerText = `Today is ${data.current.weather_descriptions}`
 
-const dataTodayIs = document.createElement('div')
-dataTodayIs.className = 'data_today-is'
+  const dataWind = document.createElement('div')
+  dataWind.className = 'data_wind'
+  dataWind.innerText = `Wind: ${data.current.wind_dir}`
 
-const dataWind = document.createElement('div')
-dataWind.className = 'data_wind'
+  const dataSpeed = document.createElement('div')
+  dataSpeed.className = 'data_speed'
+  dataSpeed.innerText = `Speed: ${data.current.wind_speed} km/h`
 
-const dataSpeed = document.createElement('div')
-dataSpeed.className = 'data_speed'
+  const dataPressure = document.createElement('div')
+  dataPressure.className = 'data_pressure'
+  dataPressure.innerText = `Pressure: ${data.current.pressure} MB`
 
-const dataPressure = document.createElement('div')
-dataPressure.className = 'data_pressure'
+  weatherData.append(dataTime, dataFeelsLike, dataTodayIs, dataWind, dataSpeed, dataPressure)
+  document.getElementById('weather_container').append(weatherImg, weatherTemperature, weatherLocation, weatherData)
+  container.append(document.getElementById('weather_container'))
+}
 
-weatherData.append(dataTime, dataFeelsLike, dataTodayIs, dataWind, dataSpeed, dataPressure)
+const removeWeather = () => {
+  document.getElementById('weather_container').innerHTML = ''
+}
 
-const getWeather = (event) => {
+const getWeather = (city, country) => {
   const inputCityValue = document.querySelector('input[name=city]').value
   const inputCountryValue = document.querySelector('input[name=country]').value
 
-  event.preventDefault()
+  const error = document.querySelector('.weather_error')
 
-  const accessKey = '4c1b27c3131aecf9a0985bb9e8405b94'
+  const accessKey = '275648e5d6c0cd4333b290867b451363'
 
   if (!inputCityValue || !inputCountryValue) {
-    weatherError.style.display = 'block'
-    weatherError.innerText = 'Error. Please fill in both lines'
-    weatherInfo.style.display = 'none'
+    error.style.display = 'block'
   } else {
-    weatherError.style.display = 'none'
-    weatherInfo.style.display = 'block'
-    fetch(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${inputCityValue},${inputCountryValue}`)
+    error.style.display = 'none'
+    fetch(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${city},${country}`)
       .then((res) => res.json())
-      .then((res) => {
-        weatherImg.style.background = `url(${res.current.weather_icons})`
-        weatherTemperature.innerText = `${res.current.temperature}°C`
-        weatherLocation.innerText = `${res.location.name}, ${res.location.country}`
-        dataTime.innerText = `${res.current.observation_time}`
-        dataFeelsLike.innerText = `Feels like: ${res.current.feelslike}°C`
-        dataTodayIs.innerText = `Today is ${res.current.weather_descriptions}`
-        dataWind.innerText = `Wind: ${res.current.wind_dir}`
-        dataSpeed.innerText = `Speed: ${res.current.wind_speed} km/h`
-        dataPressure.innerText = `Pressure: ${res.current.pressure} MB`
-      }).catch(() => {
+      .then((data) => {
+        removeWeather()
+        renderWeather(data)
+      })
+      .catch(() => {
         console.log('Error')
       })
   }
 }
 
-button.addEventListener('click', getWeather)
+const button = document.querySelector('.weather_submit')
+button.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const city = document.querySelector('input[name=city]').value
+  const country = document.querySelector('input[name=country]').value
+  getWeather(city, country)
+})
